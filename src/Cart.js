3da56909055985af952d9f809f1f70f9cf950d57
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-12-14 16:13:00 +0530

    learned to render a list of item in react

--- src/Cart.js
+++ src/Cart.js
@@ -5,11 +5,15 @@ import CartItem from './CartItem';
 
 class Cart extends React.Component {
     render () {
+        const arr = [1,2,3,4,5];
         return (
             <div className='cart'>
+                {/* <CartItem />
                 <CartItem />
-                <CartItem />
-                <CartItem />
+                <CartItem /> */}
+                {arr.map((item) => {
+                    return item
+                })}
             </div>
         );
     }
