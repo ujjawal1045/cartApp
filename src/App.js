dd980e2d05374804b9ec1a6e7db42188c9ba7f38
ujjawal1045 <90901525+ujjawal1045@users.noreply.github.com>
2022-12-27 15:16:07 +0530

    created nav bar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CartItem from './CartItem';
 import Cart from './Cart';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Cart/>
     </div>
   );
